c767c4d8ee5e8db5104469cbe44bd796ef8f01b6
PradnyaThite <123830520+PradnyaThite@users.noreply.github.com>
2023-01-31 23:16:47 +0530

    Add files via upload

---
+++ TASK_2/script.js
@@ -0,0 +1,11 @@
+var hamburger = document.querySelector(".humb");
+var navlist = document.querySelector(".nav-list");
+var links = document.querySelector(".nav-list li");
+ 
+humburger.addEventListener("click",function(){
+    this.classList.toggle("click");
+    navlist.classList.toggle("open");
+});
+
+
+
